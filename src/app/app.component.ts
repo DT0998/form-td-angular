1a5980c14fd72b8dee38c3ca7be9678df2f475cd
DT0998 <duongkytrieuvn@gmail.com>
2023-07-22 19:11:29 +0700

    feat: add smth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,29 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    // this way make override all form
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: '',
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male',
+    // });
+
+    // this way dont make override all form
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName,
+      },
+    });
   }
   onSubmit() {
     console.log(this.signupForm);
